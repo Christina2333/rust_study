32665d82dfb9c8d5ab58ad3aae67aef1e5d3f87a
zhuwenyan02 <zhuwenyan02@meituan.com>
2022-01-07 17:21:00 +0800

    add cp_10 trait

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+
+pub mod cp10_trait;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use cp10_trait::summary::Summary;
+
+mod cp10_trait;
+
 fn main() {
-    println!("Hello, world!");
+
+    let tweet = cp10_trait::summary::Tweet {
+        username: String::from("christina"),
+        content: String::from("想下班"),
+        reply: false,
+        reweet: false
+    };
+    println!("1 new tweet {}", tweet.summarize());
+
 }
