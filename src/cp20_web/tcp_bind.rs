bbb321a5196a7db558eb240299838c172f2a18f1
zhuwenyan02 <zhuwenyan02@meituan.com>
2022-01-19 17:16:36 +0800

    cp20

--- src/cp20_web/tcp_bind.rs
+++ src/cp20_web/tcp_bind.rs
@@ -1,14 +1,14 @@
+use std::net::TcpListener;
+
 #[cfg(test)]
 mod test {
-    use std::net::TcpListener;
 
-    #[test]
-    fn tcp_bind() {
-        let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+}
 
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            println!("Connection established");
-        }
+pub fn tcp_bind() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate rust_study;
+
+use rust_study::cp20_web::tcp_bind;
+
 fn main() {
 
+    tcp_bind::tcp_bind();
 }
