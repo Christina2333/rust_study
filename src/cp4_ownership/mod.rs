841f029d1baf9ebfd861beec47dff3c757c0a31e
zhuwenyan02 <zhuwenyan02@meituan.com>
2022-01-23 16:32:22 +0800

    认识所有权

--- src/cp4_ownership/mod.rs
+++ src/cp4_ownership/mod.rs
@@ -1,10 +1,26 @@
 #[cfg(test)]
 mod test {
+
+    // 变量和数据的交互方式
+    // (1)move
     #[test]
     fn data_move() {
         let x = 5;
-        let y = x;
+        let y = x; // 实际上是复制
         println!("x={}", x);
         println!("y={}", y);
+
+        let s = String::from("test");
+        let s1 = s; // 此时s的所有权被释放。
+        // println!("s={}", s); // 会报错
+        println!("s1={}", s1);
+    }
+    // (2)clone
+    #[test]
+    fn data_clone() {
+        let s = String::from("test");
+        let s1 = s.clone();
+        println!("s={}", s);
+        println!("s1={}", s1);
     }
 }
